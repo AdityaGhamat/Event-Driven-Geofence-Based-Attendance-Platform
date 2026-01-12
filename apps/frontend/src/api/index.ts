47fd604f0c0e69b249f0e4579c7ea972137ece0f
AdityaGhamat <adityaghamat01@gmail.com>
2026-01-13 02:22:51 +0530

    feat:added backend url in frontend.

--- apps/frontend/src/api/index.ts
+++ apps/frontend/src/api/index.ts
@@ -7,7 +7,7 @@ import {
 import type { ApiResponse } from "../types";
 
 export const api = axios.create({
-  baseURL: `import.meta.env.VITE_API_URL`,
+  baseURL: `https://event-driven-geofence-based-attendance.onrender.com`,
   withCredentials: true,
 });
 
